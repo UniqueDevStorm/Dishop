f47e3febab281bffc7ad5336319b428da2ed012f
unknown <storm@uniquecode.team>
2021-01-21 13:23:02 +0900

    Feat: Restart

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import 'nprogress/nprogress.css'
 import NProgress from 'nprogress'
 import Router from "next/router";
+import 'semantic-ui-css/semantic.min.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeError', () => NProgress.done())
@@ -9,7 +10,21 @@ Router.events.on('routeChangeComplete', () => NProgress.done())
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className="bg-gray-500 text-white" style={{minHeight: '100vh'}}>
+    <div className="fixed top-0 left-0 w-full flex h-32 px-12" style={{alignItems:'center'}}>
+      {/* LEFT */}
+      <div>
+        <div className="text-4xl">DISSHOP</div>
+      </div>
+      <div className="flex-grow"/>
+    {/*  RIGHT*/}
+    <div>
+      RIGHT MENU
+    </div>
+    </div>
+    <div className="w-full h-32"/>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,11 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-import 'semantic-ui-css/semantic.min.css';
+import React from 'react';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-          <div style={{position: 'fixed', width: '100vw', top: 0, left: 0}} className="flex p-2 text-xl">
-              <div>
-                  DisShop By Team.Bit
-              </div>
-              <div className="flex-grow"/>
-              <div className="nav__right">
-                  <Link href='/'>
-                      <a>LINK1</a>
-                  </Link>
-                  <Link href='/'>
-                      <a>LINK2</a>
-                  </Link>
-                  <Link href='/'>
-                      <a>LINK3</a>
-                  </Link>
-                  <Link href='/'>
-                      <a>LINK4</a>
-                  </Link>
-              </div>
-          </div>
-
-        <h1 className={styles.title}>
-          DisShop
-        </h1>
-
-        <p className={styles.description} style={{marginTop: '1vh'}}>
-          거래를 더 쉽고 빠르게. DisShop
-        </p>
-
-        <div className={styles.grid}>
-          <a href="/" className={styles.card}>
-            <h2 style={{width: '20vh', textAlign: 'center'}}>둘러보기</h2>
-          </a>
+const Home = () => {
+    return (
+        <div>
+            ㅁㄴㅇㄹ
         </div>
-      </main>
+    );
+};
 
-      <footer className={styles.footer}>
-        <h3>
-            <a href="https://stormdev.club" target="_blank" rel="noopener noreferrer" style={{color: 'black'}}>
-              Powered by STORM
-            </a>
-        </h3>
-      </footer>
-    </div>
-  )
-}
+export default Home;
