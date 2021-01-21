846790f31136feb09e7beeb146dea1a6470bbeac
unknown <storm@uniquecode.team>
2021-01-21 11:00:19 +0900

    feat: 채신기술

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import '../styles/globals.css'
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
+import Router from "next/router";
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeError', () => NProgress.done())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Menu } from 'semantic-ui-react';
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 import 'semantic-ui-css/semantic.min.css';
 
 export default function Home() {
@@ -13,6 +12,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+          <div style={{position: 'fixed', width: '100vw', top: 0, left: 0}} className="flex p-2 text-xl">
+              <div>
+                  DisShop By Team.Bit
+              </div>
+              <div className="flex-grow"/>
+              <div className="nav__right">
+                  <Link href='/'>
+                      <a>LINK1</a>
+                  </Link>
+                  <Link href='/'>
+                      <a>LINK2</a>
+                  </Link>
+                  <Link href='/'>
+                      <a>LINK3</a>
+                  </Link>
+                  <Link href='/'>
+                      <a>LINK4</a>
+                  </Link>
+              </div>
+          </div>
 
         <h1 className={styles.title}>
           DisShop
@@ -31,8 +50,8 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <h3>
-            <a href="https://vercel.com" target="_blank" rel="noopener noreferrer" style={{color: 'black'}}>
-              Powered by Vercel
+            <a href="https://stormdev.club" target="_blank" rel="noopener noreferrer" style={{color: 'black'}}>
+              Powered by STORM
             </a>
         </h3>
       </footer>
